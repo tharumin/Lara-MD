ae16f49df8d399cd8403053d734e6da65ca4ad57
tharumin <94857655+tharumin@users.noreply.github.com>
2024-12-14 22:02:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -107,14 +107,19 @@ let tek = `
     }
     })
     conn.ev.on('creds.update', saveCreds)  
-    
-    conn.ev.on('messages.upsert', async(mek) => {
-    mek = mek.messages[0]
-    if (!mek.message) return	
-    mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-    if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
-    await conn.readMessages([mek.key])
-    }
+        
+conn.ev.on('messages.upsert', async(mek) => {
+mek = mek.messages[0]
+if (!mek.message) return
+mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+await conn.readMessages([mek.key])
+const user = mek.key.participant
+const text = `Hi`
+await conn.sendMessage(user, { text: text }, { quoted: mek })
+await isReact(mek.key, '😁') // 
+}   
+
     const m = sms(conn, mek)
     const type = getContentType(mek.message)
     const content = JSON.stringify(mek.message)
