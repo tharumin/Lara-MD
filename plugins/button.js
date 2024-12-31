21340455b39acd11c63cddf50c92c1bd4f6e5c47
tharumin <94857655+tharumin@users.noreply.github.com>
2024-12-31 17:30:49 +0530

    Update button.js

--- plugins/button.js
+++ plugins/button.js
@@ -2,54 +2,6 @@ const { cmd, commands } = require('../command');
 const yts = require('yt-search');
 const config = require('../config');
 const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, getBinaryNodeChildren, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType, downloadContentFromMessage} = require('@whiskeysockets/baileys');
-const botNumber = XeonBotInc.decodeJid(XeonBotInc.user.id)
-        const XeonTheCreator = isOwner = [botNumber, ...owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
-		const body = (m.type === 'conversation') ? m.message.conversation : (m.type == 'imageMessage') ? m.message.imageMessage.caption : (m.type == 'videoMessage') ? m.message.videoMessage.caption : (m.type == 'extendedTextMessage') ? m.message.extendedTextMessage.text : (m.type == 'buttonsResponseMessage') ? m.message.buttonsResponseMessage.selectedButtonId : (m.type == 'listResponseMessage') ? m.message.listResponseMessage.singleSelectReply.selectedRowId : (m.type == 'templateButtonReplyMessage') ? m.message.templateButtonReplyMessage.selectedId : (m.type === 'messageContextInfo') ? (m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text) : ''
-		const budy = (typeof m.text == 'string' ? m.text : '')
-		const prefix = /[\uD800-\uDBFF][\uDC00-\uDFFF]/gi.test(body) ? body.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/gi)[0] : /^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@()#,'"*+÷/\%^&.©^]/gi.test(body) ? body.match(/^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@()#,'"*+÷/\%^&.©^]/gi)[0] : '.'
-		const isCmd = body.startsWith(prefix)
-		//prefix 2
-        const pric = /^#.¦|\\^/.test(body) ? body.match(/^#.¦|\\^/gi) : xprefix
-        const xeonybody = body.startsWith(pric)
-        const isCommand = xeonybody ? body.replace(pric, '').trim().split(/ +/).shift().toLowerCase() : ""
-		const args = body.trim().split(/ +/).slice(1)
-		const getQuoted = (m.quoted || m)
-		const quoted = (getQuoted.type == 'buttonsMessage') ? getQuoted[Object.keys(getQuoted)[1]] : (getQuoted.type == 'templateMessage') ? getQuoted.hydratedTemplate[Object.keys(getQuoted.hydratedTemplate)[1]] : (getQuoted.type == 'product') ? getQuoted[Object.keys(getQuoted)[0]] : m.quoted ? m.quoted : m
-		const command = XeonTheCreator ? body.replace(prefix, '').trim().split(/ +/).shift().toLowerCase() : isCmd ? body.replace(prefix, '').trim().split(/ +/).shift().toLowerCase() : ''
-		const text = q = args.join(' ')
-		const mime = (quoted.msg || quoted).mimetype || ''
-		const qmsg = (quoted.msg || quoted)
-		const sender = m.sender
-		const isUser = xeonverifieduser.includes(sender)
-		const groupMetadata = m.isGroup ? await XeonBotInc.groupMetadata(m.chat).catch(e => {}) : ''
-		const participants = m.isGroup ? await groupMetadata.participants : ''
-		if (m.isGroup) {
-			m.metadata = await XeonBotInc.groupMetadata(m.chat)
-			m.admins = (m.metadata.participants.reduce((a, b) => (b.admin ? a.push({ id: b.id, admin: b.admin }) : [...a]) && a, []))
-			m.isAdmin = m.admins.some((b) => b.id === m.sender)
-			m.participant = m.key.participant
-			m.isBotAdmin = !!m.admins.find((member) => member.id === botNumber)
-		}
-		
-		//media
-        const isMedia = /image|video|sticker|audio/.test(mime)
-        const isImage = (type == 'imageMessage')
-        const isVideo = (type == 'videoMessage')
-        const isAudio = (type == 'audioMessage')
-        const isDocument = (type == 'documentMessage')
-        const isLocation = (type == 'locationMessage')
-        const isContact = (type == 'contactMessage')
-        const isSticker = (type == 'stickerMessage')
-        const isText = (type == 'textMessage')
-        const isQuotedText = type === 'extendexTextMessage' 
-        const isQuotedImage = type === 'extendedTextMessage' 
-        const isQuotedLocation = type === 'extendedTextMessage' 
-        const isQuotedVideo = type === 'extendedTextMessage' 
-        const isQuotedSticker = type === 'extendedTextMessage'
-        const isQuotedAudio = type === 'extendedTextMessage' 
-        const isQuotedContact = type === 'extendedTextMessage' 
-        const isQuotedDocument = type === 'extendedTextMessage'
-
 cmd({
     pattern: "sadee",
     desc: "button test",
@@ -125,7 +77,7 @@ let msg = generateWAMessageFromContent(from, {
     }
   }
 }, { quoted: mek })
-await XeonBotInc.relayMessage(msg.key.remoteJid, msg.message, {
+await conn.relayMessage(msg.key.remoteJid, msg.message, {
   messageId: msg.key.id
 })
     } catch (e) {
