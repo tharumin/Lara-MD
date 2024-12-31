0dc98b7bee4a5c87adaa03932fcd5c98c2c84026
tharumin <94857655+tharumin@users.noreply.github.com>
2024-12-31 15:20:08 +0530

    Add files via upload

---
+++ plugins/button.js
@@ -0,0 +1,95 @@
+const { cmd, commands } = require('../command');
+const yts = require('yt-search');
+
+cmd({
+    pattern: "sadee",
+    desc: "button test",
+    react: "🎵",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!q) return reply("Please give me a title.");
+        let search = await yts(text);
+        let videos = search.all;
+        console.log(videos)
+        if (!videos || videos.length === 0) {
+          reply('No video found');
+          return;
+        }
+        // Choose between 1 and 5 videos at random
+        const numVideos = Math.min(videos.length, Math.floor(Math.random() * 10) + 1);
+        const selectedVideos = [];
+        while (selectedVideos.length < numVideos) {
+          const randomIndex = Math.floor(Math.random() * videos.length);
+          const randomVideo = videos.splice(randomIndex, 1)[0]; // Avoid selecting the same videos
+          selectedVideos.push(randomVideo);
+        }
+        let push = [];
+        for (let i = 0; i < selectedVideos.length; i++) {
+          let video = selectedVideos[i];
+          let cap = `Title : ${video.title}`;
+          const mediaMessage = await conn.sendMessage(from, {image: { url: video.thumbnail} });
+          push.push({
+            body: proto.Message.InteractiveMessage.Body.fromObject({
+              text: cap
+            }),
+            footer: proto.Message.InteractiveMessage.Footer.fromObject({
+              text: botname
+            }),
+            header: proto.Message.InteractiveMessage.Header.create({
+              title: `Video ${i + 1}`,
+              subtitle: '',
+              hasMediaAttachment: true,
+              ...mediaMessage
+            }),
+            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
+              buttons: [
+                {
+                  "name": "cta_copy",
+                  "buttonParamsJson": `{"display_text":"Copy Url","id":"1234","copy_code":"${video.url}"}`
+                }
+              ]
+            })
+          });
+        }
+        const msg = conn.sendMessage(m.chat, {
+          viewOnceMessage: {
+            message: {
+              messageContextInfo: {
+                deviceListMetadata: {},
+                deviceListMetadataVersion: 2
+              },
+              interactiveMessage: proto.Message.InteractiveMessage.fromObject({
+                body: proto.Message.InteractiveMessage.Body.create({
+                  text: 'sadeesha'
+                }),
+                footer: proto.Message.InteractiveMessage.Footer.create({
+                  text: 'lara-md'
+                }),
+                header: proto.Message.InteractiveMessage.Header.create({
+                  hasMediaAttachment: false
+                }),
+                carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({
+                  cards: push
+                }),
+                contextInfo: {
+                      mentionedJid: [m.sender], 
+                      forwardingScore: 999,
+                      isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                      newsletterJid: '120363192254044294@newsletter',
+                      newsletterName: 'Sadeesha',
+                      serverMessageId: 143
+                    }
+                    }
+              })
+            }
+          }
+        }, {quoted:m});
+        
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+})
