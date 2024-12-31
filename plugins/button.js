b48a1bdfc045867601f39eb4b7dbf320f9e41a1e
tharumin <94857655+tharumin@users.noreply.github.com>
2025-01-01 04:53:44 +0530

    Update button.js

--- plugins/button.js
+++ plugins/button.js
@@ -36,7 +36,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
           })
         }
         ]
-    }
+    })
     
       } catch (e) {
       console.log(e);
