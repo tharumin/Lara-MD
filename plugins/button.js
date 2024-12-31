0e51d995f55a23addf2ce6d0e101e773100f8e4d
tharumin <94857655+tharumin@users.noreply.github.com>
2024-12-31 15:24:43 +0530

    Update button.js

--- plugins/button.js
+++ plugins/button.js
@@ -5,6 +5,7 @@ cmd({
     pattern: "sadee",
     desc: "button test",
     react: "🎵",
+    category: "download",
     filename: __filename
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
