17e18706e42e5a8ab40139ce5836011744e8f3b0
tharumin <94857655+tharumin@users.noreply.github.com>
2025-01-01 05:00:16 +0530

    Update button.js

--- plugins/button.js
+++ plugins/button.js
@@ -9,7 +9,7 @@ cmd({
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
   try {
-    await conn.sendMessage(from,{
+    await conn.sendMessage(m.id,{
       title: "Sadeesha",
       text: "Hi",
       buttons: [
