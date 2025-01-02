02b21bc1b49e1cb1cffd09f98c6c804dc3c89fc2
tharumin <94857655+tharumin@users.noreply.github.com>
2025-01-02 10:00:57 +0530

    Update button.js

--- plugins/button.js
+++ plugins/button.js
@@ -118,7 +118,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         });
     console.log('Button Send Sucsses');
 
-        const sadeeID = msg.message.mek.id;
+        const sadeeID = msg.message.mek;
         conn.ev.on('messages.upsert', async (messageUpdate) => {
             const mek = messageUpdate.messages[0];
             if (!mek.message) return;
