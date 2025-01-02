e5cca964b36ccc15a9969b3243495bcdffe6e732
tharumin <94857655+tharumin@users.noreply.github.com>
2025-01-02 11:25:24 +0530

    Update button.js

--- plugins/button.js
+++ plugins/button.js
@@ -187,7 +187,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
     try {
 let cap = `Title`;
           let foot = `© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ꜱᴀᴅᴇᴇꜱʜᴀ ᴄᴏᴅᴇʀ · · ·`;
-          const mediaMessage = await prepareWAMessageMedia({ image: { url: video.thumbnail } }, { upload: conn.waUploadToServer });
+          const mediaMessage = await prepareWAMessageMedia({ image: { url: `https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/daenerys%20targaryen.jpeg` } }, { upload: conn.waUploadToServer });
           push({
             body: proto.Message.InteractiveMessage.Body.fromObject({
               text: cap
